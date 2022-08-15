231fcfc2e987b7cbd09044bd905c5d7692b922ca
BubbleGumWarior <bubblegumwarior@gmail.com>
2022-08-16 00:42:56 +0200

    Redid login Screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 
 @Component({
@@ -8,16 +8,34 @@ import {Router} from "@angular/router";
 })
 
 export class LoginComponent implements OnInit {
-  userName = "";
-  userPassword = "";
-
+  userName: string;
+  password: string;
   constructor(private router: Router) {
   }
 
   ngOnInit(): void {
   }
+  toggleLogin() {
+    const list = document.querySelectorAll('.container');
+    list.forEach((item) =>
+      item.classList.toggle('active'));
+  }
+
+  logIn() {
+    console.log(this.userName);
+    console.log(this.password);
+
+    if (this.userName === 'Bubble' && this.password === '1234'){
+      this.router.navigate(['mainScreen'])
+    }
+    else {
+      this.userName = "";
+      this.password = "";
+    }
+  }
 
   onLogin(e: any) {
+    console.log("Test")
     e.preventDefault();
     var userName = e.target.elements[0].value;
     var password = e.target.elements[1].value;
