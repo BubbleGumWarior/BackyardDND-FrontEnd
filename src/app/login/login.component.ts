b26ad731b3c0ee1ff8cff1e65113f4730aa2c85e
BubbleGumWarior <bubblegumwarior@gmail.com>
2022-08-23 21:04:36 +0200

    Finished board

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,8 @@ import {Router} from "@angular/router";
 export class LoginComponent implements OnInit {
   userName: string;
   password: string;
+  email: string;
+  confirmPassword: string;
   constructor(private router: Router) {
   }
 
@@ -31,6 +33,43 @@ export class LoginComponent implements OnInit {
     else {
       this.userName = "";
       this.password = "";
+    }
+  }
+
+  validateEmail(mail:string){
+    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(mail);
+  }
+
+  createAccount($event: SubmitEvent) {
+    const errors = [];
+
+    if (typeof this.userName === "undefined" || typeof this.email === "undefined" || typeof this.password === "undefined" || typeof this.confirmPassword === "undefined"){
+      errors.push("Please fill in all the fields!")
+    }
+    else{
+      if (this.userName.length < 6) {
+        errors.push("Username is too short!")
+      }
+      if (!this.validateEmail(this.email)) {
+        errors.push("Invalid Email!")
+      }
+      if (this.password != this.confirmPassword){
+        errors.push("Passwords do not match!")
+      }
+      if (this.password.length < 10){
+        errors.push("Password is too short!")
+      }
+    }
+    if (errors.length > 0){
+      let errorMessage = "";
+      errors.forEach(function (message) {
+        errorMessage += '\n' + message;
+      })
+      alert(errorMessage);
+    }
+    else{
+      console.log(this.userName, this.password, this.confirmPassword, this.email);
+
     }
   }
 }
